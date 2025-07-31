45286cb4b8d94026bfab821b7b9da7d514451fcf
nomadremdev <nomadrem.dev@gmail.com>
2025-08-01 00:03:29 +0100

    fix: vercel deployment issue

--- src/hooks/useRecipients.ts
+++ src/hooks/useRecipients.ts
@@ -1,5 +1,7 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "sonner";
+import {
+  useQuery /*useMutation, useQueryClient*/,
+} from "@tanstack/react-query";
+// import { toast } from "sonner";
 
 import { recipientsService } from "@/services/recipients";
 // import { ROUTES } from "@/constants/routes";
