7d7d6b25a2bcf35dbdbdca3045ebb0af5dc95418
nomadremdev <nomadrem.dev@gmail.com>
2025-09-11 00:10:00 +0100

    fix: vercel deployment issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ROUTES } from "../constants/routes";
 import PrivateRoute from "./PrivateRoute";
 
